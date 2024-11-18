a1ec9aecaedde581d46d954a66b449ae86c10fc8
Tiyasha Kundu <tiyashakundu3@gmail.com>
2024-11-19 05:11:32 +0530

    updated with signing button
    Few features changed by Rainee and Udisha

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '../button';
 import { Link } from 'react-router-dom';
-import Header from '../custom/Header'; // Assuming you have a Header component
+//import Header from '../custom/Header'; // Assuming you have a Header component
 
 function Hero() {
   return (
@@ -18,7 +18,7 @@ function Hero() {
 
       {/* Translucent Box */}
       <div
-        className="relative flex flex-col items-center gap-9 p-16 rounded-xl shadow-lg mx-auto mt-20"
+        className="relative flex flex-col items-center gap-9 p-16 rounded-xl shadow-lg mx-auto mt-12"
         style={{
           backgroundImage: `url('/bg_pic2.jpg')`,
           backgroundSize: 'cover',
@@ -31,14 +31,14 @@ function Hero() {
       >
         {/* Text */}
         <h1 className="font-bold text-[40px] md:text-[60px] text-center">
-          <span className="text-[#f56551]">Personalised Itineraries at Your Fingertips</span>
+          <span className="text-[#b85c00]">Personalised Itineraries at Your Fingertips</span>
         </h1>
-        <p className="text-gray-700 text-lg text-center max-w-2xl">
+        <p className="text-gray-700 text-2xl font-bold text-center max-w-2xl">
           Discover your next adventure with tailor-made itineraries that suit your travel style and preferences.
         </p>
         {/* Button */}
         <Link to={'/create-trip'}>
-          <Button className="bg-[#f56551] text-white hover:bg-[#e53e3e] transition duration-300 ease-in-out px-6 py-3 rounded-md shadow-lg text-lg font-medium">
+          <Button className="bg-[#b85c00] text-white hover:bg-[#e53e3e] transition duration-300 ease-in-out px-6 py-3 rounded-md shadow-lg text-lg font-medium relative -top-8">
             Get Started Now
           </Button>
         </Link>
